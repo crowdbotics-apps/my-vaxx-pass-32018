e6ce5a58ab8abde14121bb64e5b1d7752183995f
Crowdbotics <team@crowdbotics.com>
2021-11-16 19:40:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "my_vaxx_pass_32018",
+  url: "https://my_vaxx_pass_32018.botics.co",
+  api: "https://my_vaxx_pass_32018.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
